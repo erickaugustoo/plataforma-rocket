492cb020c189fe6cab60940bf5b51fd9cbabf68a
erickaugustoo <erickkuiz@gmail.com>
2022-02-01 15:46:53 -0300

    update node.js #2

---
+++ public/scripts/modal.js
@@ -0,0 +1,25 @@
+export default function Modal(){
+
+  const modalWrapper = document.querySelector('.modal-wrapper')
+
+  const cancelButton = document.querySelector('.button.cancel')
+
+  cancelButton.addEventListener("click", close)
+  function open(){
+
+    //active para modal
+    modalWrapper.classList.add("active")
+  }
+  function close(){
+
+    // remover active para modal
+    modalWrapper.classList.remove("active")
+  }
+  
+  return{
+    open,
+    close
+
+  }
+
+}
